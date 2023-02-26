1e03d2950e17816e10671c7c1263dd57b37ac6cd
n132 <xm2146@nyu.edu>
2023-02-26 15:06:55 -0500

    Linux Terminal

---
+++ Linux-Terminal/README.md
@@ -0,0 +1,9 @@
+# Linux Terminal
+* I use zsh and the configure file is `~/.zshrc`.
+
+
+# Usage
+- Modify the source [code](./main.c) to replace `/root/conf/prompt` with the path of your logo/prompt
+- run command `make` to make the binary
+- Append the command `<path_to_binary>` to your shell configure file, e.g. `~/.zshrc`
+
---
+++ Linux-Terminal/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+void black() {
+	printf("\033[0;30m");
+} 
+void red () {
+  	printf("\033[1;31m");
+}
+void green() {
+	printf("\033[0;32m");
+} 
+void yellow() {
+  	printf("\033[1;33m");
+}
+void blue() {
+	printf("\033[0;34m");
+} 
+void purple() {
+	printf("\033[0;35m");
+} 
+void cyan() {
+	printf("\033[0;36m");
+} 
+void white() {
+	printf("\033[0;37m");
+} 
+void reset () {
+  	printf("\033[0m");
+}
+int DONE = 1;
+void * color[3]={
+	blue,
+	purple,
+	cyan
+};
+char * readline(int fd){
+	size_t ct = 0;
+	char *ptr = malloc(0x400);
+	memset(ptr,0,0x400);
+	while(ct < 0x400 && ptr[ct-1]!='\n')
+	{
+		size_t res = read(fd,ptr+ct,1);
+		if(res!=1)
+		{
+			DONE = 0;
+			break;
+		}
+		ct+=1;
+	}
+	return ptr;
+}
+void colorize_file(char *file_name){
+	
+	int fd = open(file_name,0);
+	if(fd<0)
+		puts("open err");
+	else{
+		char *s=0;
+		while((s=readline(fd),DONE))
+		{
+			// printf("%p\n",s);
+			int r = rand() % 3;
+			void (*chosen)() = color[r];
+			chosen();
+			printf("%s",s);
+			// reset();
+		}
+	}	
+	close(fd);
+}
+int main () {
+  	srand(time(NULL));
+  	colorize_file("/root/conf/prompt");
+	printf("\n");
+  	return 0;
+}
